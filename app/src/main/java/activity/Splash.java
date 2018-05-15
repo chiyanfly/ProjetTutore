f4aab3a14ddd4883a331ba351777753bcc2b0aab
Guangjie <gzhang@etud.insa-toulouse.fr>
2018-05-15 13:24:10 +0200

    For testing

---
+++ app/src/main/java/activity/Splash.java
@@ -0,0 +1,28 @@
+package activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.ressources.R;
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH=3000;//延迟三秒
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent=new Intent(Splash.this, SortActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
+    }
+
+
+}
